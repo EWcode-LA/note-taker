5bb2936e46f293b6e51e6dc322967c4ad16ab620
EWcode-LA <seth.miller87@proton.mail>
2021-02-16 20:32:12 -0800

    deleteroute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,69 +1,90 @@
-const fs = require('fs');
-const path = require('path');
-const { v4 : uuidv4 } = require('uuid');
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-    let notes = [];
+module.exports = function (app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+  let notes = [];
 
-    // fs.readFile(path.join(__dirname, "./db/db.json"), 'utf8' , (err, data) => {
-    //   if (err) {
-    //     console.error(err)
-    //     return
-    //   }
-    //   notes = JSON.parse(data);
-    //   // notes.push({title:"my test", text:"my text"})
-    //   console.log(notes);
-    // })
-    app.get("/api/notes", function(req, res) {
-      fs.readFile(path.join(__dirname, "../db/db.json"), 'utf8' , (err, data) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        notes = JSON.parse(data);
-        // notes.push({title:"my test", text:"my text"})
-        console.log(notes);
-      })
-      res.json(notes);
-    });
-//add post and delete
-
-// //new object
-//   {
-//     id: <insertId></insertId>
-//     title: req.body.title
-//     text: req.body.text
-//   }
-
-    app.post("/api/notes", function(req, res) {
-      let note = {
-        id: uuidv4(),
-            title: req.body.title,
-            text: req.body.text
-      };
-      notes.push(note);
-      fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes) , (err) => {
-        if (err) {
-          console.error(err)
-          return
-        }
+  // fs.readFile(path.join(__dirname, "./db/db.json"), 'utf8' , (err, data) => {
+  //   if (err) {
+  //     console.error(err)
+  //     return
+  //   }
+  //   notes = JSON.parse(data);
+  //   // notes.push({title:"my test", text:"my text"})
+  //   console.log(notes);
+  // })
+  app.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      notes = JSON.parse(data);
+      // notes.push({title:"my test", text:"my text"})
       
+    });
+    res.json(notes);
+  });
+  //add post and delete
+
+  // //new object
+  //   {
+  //     id: <insertId></insertId>
+  //     title: req.body.title
+  //     text: req.body.text
+  //   }
+
+  app.post("/api/notes", function (req, res) {
+    let note = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+    notes.push(note);
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
         // notes.push({title:"my test", text:"my text"})
-        console.log(notes);
+        
         res.send(200);
-      })
+      }
+    );
+  });
+  //api/notes or ../db/db.json?
+  //I want to delete the id
+  app.delete("/api/notes/:id", function (req, res) {
+    // note.id.length = 0;
+    const noteId = req.params.id;
+    const noteIndex = notes.findIndex(function (note) {
+      return note.id === noteId;
     });
+    notes.splice(noteIndex, 1);
 
-    app.delete("/api/notes", function(req, res) {
-      note.id.length = 0;
-      // Empty out the arrays of data
-      res.json({ ok: true });
-    });
+    fs.writeFile(
+      path.join(__dirname, "../db/db.json"),
+      JSON.stringify(notes),
+      (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        // notes.push({title:"my test", text:"my text"})
+
+        res.send(200);
+      }
+    );
+  });
 };
-
-//add post and delete
